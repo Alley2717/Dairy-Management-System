5eb373c00b01ce476423875f57eb220ed989545a
not-ad-chaos <chaotic10101@gmail.com>
2025-04-04 09:26:27 +0530

    Initial

---
+++ frontend/src/app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+    return (
+        <div class="container parent">
+            <div class="container">
+                <img class="img img1" src="yellow.png" />
+                <img class="img img2" src="red.png" />
+            </div>
+            <img class="img img5" src="blue.png" />
+            <div class="container bottom">
+                <img class="img img3" src="brown.png" />
+                <img class="img img4" src="milking.png" />
+            </div>
+        </div>
+    );
+}
